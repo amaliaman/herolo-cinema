113c2302037b7e91bab481438e680f7e0cfdf0e4
Amalia Man <amaliagilad@gmail.com>
2018-11-11 19:49:55 +0200

    Build

---
+++ build/precache-manifest.cb77d174440047808dc167de4ca228e3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "546bb2e79b0c0e3c9e128a55f48de5da",
+    "url": "/static/media/loader.546bb2e7.svg"
+  },
+  {
+    "revision": "d5777eece98f59d5c1047e4a156e9db6",
+    "url": "/static/media/bg.d5777eec.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e7ca519ee39d19d96d79",
+    "url": "/static/js/main.e7ca519e.chunk.js"
+  },
+  {
+    "revision": "55ab622eceb33b5c0cc1",
+    "url": "/static/js/1.55ab622e.chunk.js"
+  },
+  {
+    "revision": "e7ca519ee39d19d96d79",
+    "url": "/static/css/main.a96f08af.chunk.css"
+  },
+  {
+    "revision": "55ab622eceb33b5c0cc1",
+    "url": "/static/css/1.e79bbcc9.chunk.css"
+  },
+  {
+    "revision": "a1988225470a27fdafc3ef23e00b1868",
+    "url": "/index.html"
+  }
+];
